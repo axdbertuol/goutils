fd1e96f720aeb8073338cc9a9ce2fd224f26f5f0
axdbertuol <axdber@gmail.com>
2024-02-10 21:03:56 -0300

    feat: middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,20 @@
+package custom_middleware
+
+import (
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+
+	"github.com/labstack/echo/v4"
+)
+
+type DefaultValidator struct {
+	Validator *validator.Validate
+}
+
+func (cv *DefaultValidator) Validate(i interface{}) error {
+	if err := cv.Validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
